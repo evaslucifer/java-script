ac3389400eb77e1ecdc5ccc349b9787e24cb2ca6
evaslucifer <himanish772005@gmail.com>
2025-03-15 20:04:59 +0530

    3

--- js.js
+++ js.js
@@ -1,1 +1,33 @@
 //hello
+// data types
+// int float double 
+//charcter
+//null
+//undefined
+
+//keywords
+
+//variables
+//its a container box
+//storing data types
+//syntax
+//var jfdcn=25;
+
+// var jfdcn=null;   //null data types
+ //var jfdcn;        //undefined 
+
+ //string data type
+ //>> var name="hmainsh"
+ // 
+
+ //>> const n="23";    //it can be edited //const
+ var age=25;
+ console.log(age);
+
+ // non primitive data type
+ // arrays,objects
+ 
+ //object synyax
+ // var objectName{
+ // key:value
+ //}
