7ae1661adbac831425b31cd1d32f2d50d565778f
evaslucifer <himanish772005@gmail.com>
2025-03-27 15:41:55 +0530

    5

--- operatorsAndArrayMethods.js
+++ operatorsAndArrayMethods.js
@@ -1,13 +1,39 @@
-// js operators
-var num1=10;
-var num2=50;
-var sum=num1+num2;
-console.log(sum);
+// // js operators
+// var num1=10;
+// var num2=50;
+// var sum=num1+num2;
+// console.log(sum);
 
-// short hand operators
- var a=2;
- var b=4;
+// // short hand operators
+//  var a=2;
+//  var b=4;
 
- console.log(a+b);
- a+=b;
- console.log(a);
+//  console.log(a+b);
+//  a+=b;
+//  console.log(a);
+
+
+// //  functions
+// var obj1={
+//     bikename:function(){
+//         console.log("we have a bike name");
+//     } 
+// }
+// obj1.bikename();
+
+
+// named functions
+
+// function sum(a ,b){
+//     console.log(a+b);
+// }
+
+// sum(2,3);
+
+
+// anonymous function
+
+var sum =function(a,b){
+    console.log(a+b);
+};
+sum(5,3);
