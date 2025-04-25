7e3f58b6c560fcd0f71faa9b00fe662fa2234447
evaslucifer <himanish772005@gmail.com>
2025-04-25 16:02:47 +0530

    2

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,6 +1,27 @@
 // async and await in js
-setTimeout(function(){
-    console.log("1");
+// setTimeout(function(){
+    // console.log("1");
 
-},3000);
-console.log("2");
+// },3000);
+// console.log("2");
+var myPromise=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve("success");   
+     },5000);
+    setTimeout(function(){
+       reject("err"); 
+    },4000);
+});
+var getourpromise=async()=>{
+    try{
+        let getourpromiseresult=await myPromise;
+        console.log(getourpromiseresult);
+    }
+        catch(error){
+        // throw new Error(error) 
+        console.log(error);
+        }
+
+    
+};
+getourpromise(); 
